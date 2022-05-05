ac049cd075ea4e0d6638944eb0c5fd2a8c8e6166
EmreKalender <93441345+EmreKalender@users.noreply.github.com>
2022-05-05 23:31:14 +0300

    Todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
-
+function WelcomeMessage(){ 
+ return <p>Welcome!</p>
+}
 function App() {
-  return (
+  let message='Hello!'
+    const [count,setCount]=useState(0)
+    const [count2,setCount2]=useState(0)
+     
+      return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p className="mt-10">
+          You clicked {count} times
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          You clicked {count2} times
+        </p>
+        <button className="mb-20" onClick={()=>setCount(count+1)}>Click me</button>
+        <button onClick={()=>setCount(0)}>Zero me</button>
+                <button onClick={()=>setCount2(count2+1)}>Click me</button>
+        <button onClick={()=>setCount2(0)}>Zero me</button>
+        </header>
+
+      </div>
+    )
+  
 }
 
 export default App;
